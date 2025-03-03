09b828c6e1431a95aabcf533af39f80d0ad30b73
pavel <pugachenkopavel72@gmail.com>
2025-03-03 13:49:08 +0500

    start

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("mailing.urls", namespace="mailing")),
-    path("users/", include("users.urls", namespace="users")),
+    # path("", include("mailing.urls", namespace="mailing")),
+    # path("users/", include("users.urls", namespace="users")),
 ]
 
 if settings.DEBUG:
